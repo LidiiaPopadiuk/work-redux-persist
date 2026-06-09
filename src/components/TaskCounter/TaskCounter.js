d6b491ae0c1af38cdb31fc730de0f4ab77bfb0c3
LidiiaPopadiuk <lidiiadolfin@gmail.com>
2026-06-09 23:55:53 +0200

    feat: doing createSelector

--- src/components/TaskCounter/TaskCounter.js
+++ src/components/TaskCounter/TaskCounter.js
@@ -1,17 +1,18 @@
 import css from "./TaskCounter.module.css";
 import { useSelector } from "react-redux";
-import { selectTodos } from "redux/todos/todosSelectors";
+// import { selectTodos } from "redux/todos/todosSelectors";
+import { selectCount } from "redux/todos/todosSelectors";
 
 export const TaskCounter = () => {
-  const tasks = useSelector(selectTodos);
-  console.log("tasks", tasks);
-  
-  const activeTasks = tasks.filter(task => !task.completed);
-  const completedTasks = tasks.filter(task => task.completed);
+  const { active, commpleted } = useSelector(selectCount);
+  // console.log("tasks", tasks);
+
+  // const activeTasks = tasks.filter(task => !task.completed);
+  // const completedTasks = tasks.filter(task => task.completed);
   return (
     <div>
-      <p className={css.text}>Active: {activeTasks.length}</p>
-      <p className={css.text}>Completed: {completedTasks.length}</p>
+      <p className={css.text}>Active: {active}</p>
+      <p className={css.text}>Completed: {commpleted}</p>
     </div>
   );
 };
--- src/redux/todos/todosSelectors.jsx
+++ src/redux/todos/todosSelectors.jsx
@@ -1,1 +1,22 @@
-export const selectTodos = (state) => state.todos.todos;  
+import { createSelector } from "@reduxjs/toolkit";
+
+export const selectTodos = (state) => state.todos.todos;
+
+export const selectCount = createSelector(
+    [selectTodos],
+    (todos) => {
+        return todos.reduce((acc, todo) => {
+            if (todo.completed) {
+                acc.commpleted += 1
+            } else {
+                acc.active += 1
+            }
+            return acc
+        },
+            {
+                active: 0,
+                commpleted: 0
+            }
+        )
+    }
+)
